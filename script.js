b373be20609b0954fae2b7cbdc6270e81eb067ad
saaramukadam3 <saaramukadam3@gmail.com>
2026-01-04 19:16:26 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,32 @@
+// console.log("Portfolio Loaded Successfully");
+// Smooth scrolling for navbar
+document.querySelectorAll("nav a").forEach((link) => {
+    link.addEventListener("click", function (e) {
+      e.preventDefault();
+      document
+        .querySelector(link.getAttribute("href"))
+        .scrollIntoView({ behavior: "smooth" });
+    });
+  });
+
+  const text = "Saara Mukadam";
+  let index = 0;
+
+  function typeEffect() {
+    if (index < text.length) {
+      document.getElementById("typewriter").innerHTML += text.charAt(index);
+      index++;
+      setTimeout(typeEffect, 120);
+    }
+  }
+  
+  typeEffect();
+const menuToggle = document.getElementById("menuToggle");
+  const navMenu = document.getElementById("navMenu");
+
+  menuToggle.addEventListener("click", () => {
+    navMenu.classList.toggle("active");
+  });
+  function closeMenu() {
+    navMenu.classList.remove("active");
+  }
